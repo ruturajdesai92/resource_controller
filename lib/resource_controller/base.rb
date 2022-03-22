7ab2b45fb743dda02160f15757f928cf40a79265
Ruturaj Desai <ruturajdesai@hotmail.com>
2022-03-22 13:51:34 +0530

    removed unloadable

--- lib/resource_controller/base.rb
+++ lib/resource_controller/base.rb
@@ -4,9 +4,7 @@ module ResourceController
   # 
   # Inherit from this class to create your RESTful controller.  See the README for usage.
   # 
-  class Base < ::ApplicationController
-    unloadable
-    
+  class Base < ::ApplicationController    
     def self.inherited(subclass)
       super
       subclass.class_eval { resource_controller }
--- lib/resource_controller/singleton.rb
+++ lib/resource_controller/singleton.rb
@@ -4,9 +4,7 @@ module ResourceController
   # 
   # Inherit from this class to create your RESTful singleton controller.  See the README for usage.
   # 
-  class Singleton < ::ApplicationController
-    unloadable
-    
+  class Singleton < ::ApplicationController    
     def self.inherited(subclass)
       super
       subclass.class_eval { resource_controller :singleton }
